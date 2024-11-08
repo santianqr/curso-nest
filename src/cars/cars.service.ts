783aafb102f1beb211821af0f6c48f8f854f6323
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2024-11-07 22:08:13 -0500

    first commit

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: 2,
+      brand: 'Toyota',
+      model: 'Camry',
+    },
+    {
+      id: 3,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) throw new NotFoundException(`Car with id ${id} not found`);
+
+    return car;
+  }
+}
